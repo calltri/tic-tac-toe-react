5bd95d943f861fbbe15416595a1d4b2c663c7ca5
Madison W. Call <madison.call122@gmail.com>
2025-01-01 22:03:36 -0800

    Have working tic tac toe. Starting work on backing out so as to implement a game history section

--- app/page.js
+++ app/page.js
@@ -2,6 +2,15 @@
 import next from 'next';
 import { useState } from 'react';
 
+/*
+TODO:
+  identify player turns
+  calculate who wins (array of win states?, checker, and updater?)
+  Store previous data
+  HAve go back thing
+
+
+*/
 
 function Square({ value, onSquareClick }) {
 
@@ -16,17 +25,56 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[a] && squares[b] === squares[a] && squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(i) {
+    // see if need to end early
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    }
+    else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -44,4 +92,22 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className='game-info'>
+        <ol></ol>
+      </div>
+    </div>
+  )
 }
